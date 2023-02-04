ab183eff387dd54a2d9061428117560c532a0da6
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2023-02-04 13:02:57 +0000

    simplify readme and formatting

--- README.md
+++ README.md
@@ -2,39 +2,32 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
-
-In the project directory, you can run:
+## Relevant scripts/ commands
 
 ### `npm start`
 
-We've already run this for you in the `Codespaces: server` terminal window below. If you need to stop the server for any reason you can just run `npm start` again to bring it back online.
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) in the built-in Simple Browser (`Cmd/Ctrl + Shift + P > Simple Browser: Show`) to view your running application.
+Runs the app in the development mode. Open [http://localhost:3000](http://localhost:3000) in the built-in Simple Browser (`Cmd/Ctrl + Shift + P > Simple Browser: Show`).
 
 ### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Launches the test runner in the interactive watch mode. See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
 
-Builds the app for production to the `build` folder.\
+Builds the app for production to the `build` folder.
 
-Your app is ready to be deployed!
+### `npm install firebase` 
 
-## Firebase setup
+Is required in case of a new codespace.
 
-### Install npm modules (just in case)
-npm install firebase
+### `npm install -g firebase-tools`
 
-npm install -g firebase-tools
+Also necessary when a new codespace is used. 
 
-### Initialize
-firebase init hosting
+### `firebase init hosting`
 
-### Deploy
-firebase deploy --only hosting:felix-xyz
+Initialize Firebase hosting.
 
-(make sure that ```"site": "felix-xyz",``` is part of firebase.json. The *build* folder shall be deployed, not the *public* folder)
+### `firebase deploy --only hosting:felix-xyz`
+
+Deploy to Firebase. Make sure that ```"site": "felix-xyz",``` is part of firebase.json. The *build* folder shall be deployed, not the *public* folder.
--- src/App.js
+++ src/App.js
@@ -1,97 +1,95 @@
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      
-      <header className="App-header">
-        
-        <img src="profile-pic.png" className="App-logo" alt="logo" /> 
-        
-        <p>
-          Hello and welcome to my professional portfolio website! 
-            
-            { /*
-            Original Design:
-            https://codepen.io/aron-tw/pen/pLJqgE
-            */
-            }
+   return (
+      <div className="App">
 
-        </p>
-      </header>
+         <header className="App-header">
 
-   <h1>
-   What I'm currently interested in.
-   </h1>
+            <img src="profile-pic.png" className="App-logo" alt="logo" />
 
-<div>
-   <img src="word_cloud.svg" alt="Word Cloud" width="90%"></img>
-</div>
+            <p>
+               Hello and welcome to my professional portfolio website!
+            </p>
+         </header>
 
-   <h1>
-   What I have done in the past.
-   </h1>
+         <h1>
+            What I'm currently interested in.
+         </h1>
 
-      <div className="flip flip-vertical">
-    <div className="front" style={{ background: "#81d8d0" }}>
-       <h2 className="text-shadow">Project 1</h2>
-    </div>
-    <div className="back" style={{ background: "#81d8d0" }}>
-       <h2>Task: Systems Development and Operation</h2>
-       <p>For a client in the pharmaceutical industry</p>
-    </div>
-</div>
-<div className="flip flip-vertical">
-    <div className="front" style={{ background: "#fff68f" }}>
-       <h2 className="text-shadow">Project 2</h2>
-    </div>
-    <div className="back" style={{ background: "#fff68f" }}>
-       <h2>Task: Cloud Foundation Design (GCP)</h2>
-       <p>For a client in the energy industry</p>
-    </div>
-</div>
-<div className="flip flip-vertical">
-    <div className="front" style={{ background: "#ffdab9" }}>
-       <h2 className="text-shadow">Project 3</h2>
-    </div>
-    <div className="back" style={{ background: "#ffdab9" }}>
-       <h2>Task: Solution Design & Software Development</h2>
-       <p>For a client in the aerospace industry</p>
-    </div>
-</div>
-<div className="flip flip-vertical">
-    <div className="front" style={{ background: "#ff7f50" }}>
-       <h2 className="text-shadow">Project 4</h2>
-    </div>
-    <div className="back" style={{ background: "#ff7f50" }}>
-       <h2>Task: AI methods evaluation and prototyping</h2>
-       <p>For a client in the logistics industry</p>
-    </div>
-</div>
-<div className="flip flip-vertical">
-    <div className="front" style={{ background: "#333333" }}>
-       <h2 className="text-shadow">Project 5</h2>
-    </div>
-    <div className="back" style={{ background: "#333333" }}>
-       <h2>Task: Consulting, database development</h2>
-       <p>For a client in the banking industry</p>
-    </div>
-</div>
-<div className="flip flip-vertical">
-    <div className="front" style={{ background: "#ff80ed" }}>
-       <h2>Project 6</h2>
-    </div>
-    <div className="back" style={{ background: "#ff80ed" }}>
-       <h2>Task: Database development</h2>
-       <p>For a client in the banking industry (yet another one)</p>
-    </div>
-</div>
+         <div>
+            <img src="word_cloud.svg" alt="Word Cloud" width="90%"></img>
+         </div>
 
-<br></br>
-<footer>© 2023 Felix Gündling. Contact me on <a href="https://www.linkedin.com/in/felix-g%C3%BCndling-4b143a1b5/"> LinkedIn</a>
-</footer>
-    </div>
-  );
+         <h1>
+            What I have done in the past.
+         </h1>
+
+         { /*
+      Original Cards Design:
+      https://codepen.io/aron-tw/pen/pLJqgE
+      */
+         }
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#81d8d0" }}>
+               <h2 className="text-shadow">Project 1</h2>
+            </div>
+            <div className="back" style={{ background: "#81d8d0" }}>
+               <h2>Task: Systems Development and Operation</h2>
+               <p>For a client in the pharmaceutical industry</p>
+            </div>
+         </div>
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#fff68f" }}>
+               <h2 className="text-shadow">Project 2</h2>
+            </div>
+            <div className="back" style={{ background: "#fff68f" }}>
+               <h2>Task: Cloud Foundation Design (GCP)</h2>
+               <p>For a client in the energy industry</p>
+            </div>
+         </div>
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#ffdab9" }}>
+               <h2 className="text-shadow">Project 3</h2>
+            </div>
+            <div className="back" style={{ background: "#ffdab9" }}>
+               <h2>Task: Solution Design & Software Development</h2>
+               <p>For a client in the aerospace industry</p>
+            </div>
+         </div>
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#ff7f50" }}>
+               <h2 className="text-shadow">Project 4</h2>
+            </div>
+            <div className="back" style={{ background: "#ff7f50" }}>
+               <h2>Task: AI methods evaluation and prototyping</h2>
+               <p>For a client in the logistics industry</p>
+            </div>
+         </div>
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#333333" }}>
+               <h2 className="text-shadow">Project 5</h2>
+            </div>
+            <div className="back" style={{ background: "#333333" }}>
+               <h2>Task: Consulting, database development</h2>
+               <p>For a client in the banking industry</p>
+            </div>
+         </div>
+         <div className="flip flip-vertical">
+            <div className="front" style={{ background: "#ff80ed" }}>
+               <h2>Project 6</h2>
+            </div>
+            <div className="back" style={{ background: "#ff80ed" }}>
+               <h2>Task: Database development</h2>
+               <p>For a client in the banking industry (yet another one)</p>
+            </div>
+         </div>
+
+         <br></br>
+         <footer>© 2023 Felix Gündling. Contact me on <a href="https://www.linkedin.com/in/felix-g%C3%BCndling-4b143a1b5/"> LinkedIn</a>
+         </footer>
+      </div>
+   );
 }
 
 export default App;
