619bdfc00f6f5b6a582bf39abfa9f83a4ae031b0
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2023-06-08 18:53:23 +0000

    Revert index.html

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); // eslint-disable-line no-unused-vars
 // const analytics = getAnalytics(app);
 
 // Initialize AOS (Animate on scroll library)
